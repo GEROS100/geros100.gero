07c035c00dbb0f4c9421d3ec791f5945124ffe4b
GEROS100 <153160640+GEROS100@users.noreply.github.com>
2023-12-06 23:34:14 -0600

    inicia

---
+++ Fiestas_Infantiles/Decoraciones/direccion.js
@@ -0,0 +1,7 @@
+function scrollimages(direction){
+    const imageslider=document.querySelider=
+    document.querySelector('.image-slider');
+    const scrollAmount=200;
+    imageslider.scrollleft += direction * scrollAmount;
+    
+}
